5b8f566985ed72ac9f3c78b3a666aa8f7eb272d6
sonnh <sonnh911.gmail.com>
2025-08-26 09:24:54 +0700

    update

--- setup/netcfg-watcher.sh
+++ setup/netcfg-watcher.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
-REQ=/etc/netplan/request.json
-CFG=/etc/netplan/01-netcfg.yaml
+NETCFG=/root/mgwp/network/netplan.apply
+REBOOT=/root/mgwp/reboot/reboot.apply
+UPGRADE=/root/mgwp/upgrade/upgrade.tag
+WATCHERDIR=/root/mgwp
+COMPOSE_DIR=/home/mira/docker
 
-inotifywait -m -e close_write "$REQ" | while read path action file; do
-    IP=$(jq -r .ip $REQ)
-    GW=$(jq -r .gw $REQ)
-    NS=$(jq -r '.ns | join(", ")' $REQ)
+inotifywait -m -r -e close_write "$WATCHERDIR" | while read path action file; do
+  case "$file" in
+    $(basename "$NETCFG"))
+        REQ=$(cat "$NETCFG" | tr -d ' \t\n')
+        if [ "$REQ" == "Request" ]; then
+          netplan apply
+          echo "Done" > $NETCFG
+        else
+          echo "netplan apply done..."
+        fi
+        ;;
+    $(basename "$UPGRADE"))
+      cd "$COMPOSE_DIR"
+      TAG=$(cat "$UPGRADE" | tr -d ' \t\n')
+      if [ -n "$TAG" ]; then
+          echo "Detected new tag: $TAG"
+          export TAG="$TAG"
 
-    cat > $CFG <<EOF
-network:
-  version: 2
-  renderer: networkd
-  ethernets:
-    eth0:
-      dhcp4: no
-      addresses:
-        - ${IP}
-      routes:
-        - to: default
-          via: ${GW}
-      nameservers:
-        addresses: [${NS}]
-EOF
-    ip addr flush dev eth0
-    netplan apply
+          echo "Pulling new image..."
+          docker compose pull
+
+          echo "Restarting container..."
+          docker compose up -d
+
+          echo "Delete unused images..."
+          docker image prune -a -f
+      fi
+      ;;
+    $(basename "$REBOOT"))
+      RB=$(cat "$REBOOT" | tr -d ' \t\n')
+      if [ "$RB" == "Request" ]; then
+          echo "Reboot" > $REBOOT
+          sleep 1
+          reboot
+      fi
+      ;;      
+  esac
 done
--- setup/setup_mira.sh
+++ setup/setup_mira.sh
@@ -13,14 +13,19 @@ cp netcfg-watcher.sh /usr/local/bin/netcfg-watcher.sh
 chmod +x /usr/local/bin/netcfg-watcher.sh
 cp setup_leds.sh /usr/local/bin/setup_leds.sh
 chmod +x /usr/local/bin/setup_leds.sh
-cp check_and_start_container.sh /usr/local/bin/check_and_start_container.sh
-chmod +x /usr/local/bin/check_and_start_container.sh
+#cp check_and_start_container.sh /usr/local/bin/check_and_start_container.sh
+#chmod +x /usr/local/bin/check_and_start_container.sh
 
 # Setup tools
 apt update  && apt install -y inotify-tools
 
 # Setup default network
-cp request.json /etc/netplan
+mkdir -p /root/mgwp/network
+touch /root/mgwp/network/netplan.apply
+mkdir -p /root/mgwp/upgrade
+touch /root/mgwp/upgrade/upgrade.tag
+mkdir -p /root/mgwp/reboot
+touch /root/mgwp/reboot/reboot.apply
 
 # Setup network monitor service
 cp netcfg-watcher.service /etc/systemd/system/netcfg-watcher.service
@@ -35,16 +40,20 @@ systemctl enable pre-docker-gpio.service
 systemctl start pre-docker-gpio.service
 
 # Setup network leds (Link + Traffic)
-#cp setup-leds.service /etc/systemd/system/setup-leds.service
-#systemctl daemon-reload
-#systemctl enable setup-leds.service
-#systemctl start setup-leds.service
+cp setup-leds.service /etc/systemd/system/setup-leds.service
+systemctl daemon-reload
+systemctl enable setup-leds.service
+systemctl start setup-leds.service
 
 # Setup crontab, monitor mira container
-(crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/check_and_start_container.sh") | crontab -
+#(crontab -l 2>/dev/null; echo "* * * * * /usr/local/bin/check_and_start_container.sh") | crontab -
 
 # Setup /dev/ttyUSB3 used for mgwp app. ModemManager use /dev/ttyUSB2 by default
 touch /etc/udev/rules.d/99-mm-ignore.rules
 echo 'KERNEL=="ttyUSB3", ENV{ID_MM_DEVICE_IGNORE}="1"' > /etc/udev/rules.d/99-mm-ignore.rules
 udevadm control --reload-rules
 udevadm trigger
+
+# Copy netplan yaml
+mv /etc/netplan/*.yaml /etc/netplan/*.yaml.bk 
+cp *.yaml /etc/netplan
